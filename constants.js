e890b4db45fbe1f88b16ef82222958e740f8c61d
Nick Baughan <nbaughan@costar.com>
2021-07-20 08:57:33 -0400

    add constants

---
+++ constants.js
@@ -0,0 +1,8 @@
+//todo: add urls for each environment
+const getServiceUrls = () => ({
+  posts: "http://localhost:4000",
+  comments: "http://localhost:4001",
+  query: "http://localhost:4002",
+});
+
+module.exports = getServiceUrls;
